25f7e464e0ef6342d35c0a440ea4c13ac81c130c
Elliot Gonzalez <elliotgrey@gmail.com>
2020-01-24 16:08:56 -0500

    added conversion of data array

--- build/index.js
+++ build/index.js
@@ -3,20 +3,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var CsvFileReader_1 = require("./CsvFileReader");
 var reader = new CsvFileReader_1.CsvFileReader('football.csv');
 reader.read();
-var MatchResult;
-(function (MatchResult) {
-    MatchResult["HomeWin"] = "H";
-    MatchResult["AwayWin"] = "A";
-    MatchResult["Draw"] = "D";
-})(MatchResult || (MatchResult = {}));
-var manUnitedWins = 0;
-for (var i = 0; i < reader.data.length; i++) {
-    var match = reader.data[i];
-    if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-        manUnitedWins++;
-    }
-    else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
-        manUnitedWins++;
-    }
-}
-console.log("Man U won " + manUnitedWins + " games");
+console.log(reader.data[0][0]);
+// let manUnitedWins = 0;
+// for (let i = 0; i < reader.data.length; i++) {
+//   let match = reader.data[i];
+//   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+//     manUnitedWins++;
+//   } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+//     manUnitedWins++;
+//   }
+// }
+// console.log(`Man U won ${manUnitedWins} games`);
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,18 @@
 import { CsvFileReader } from './CsvFileReader';
+import { MatchResult } from './MatchResult';
 
 const reader = new CsvFileReader('football.csv');
 reader.read();
+console.log(reader.data[0][0]);
 
-enum MatchResult {
-  HomeWin = 'H',
-  AwayWin = 'A',
-  Draw = 'D'
-}
+// let manUnitedWins = 0;
+// for (let i = 0; i < reader.data.length; i++) {
+//   let match = reader.data[i];
+//   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+//     manUnitedWins++;
+//   } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+//     manUnitedWins++;
+//   }
+// }
 
-let manUnitedWins = 0;
-for (let i = 0; i < reader.data.length; i++) {
-  let match = reader.data[i];
-  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-    manUnitedWins++;
-  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
-    manUnitedWins++;
-  }
-}
-
-console.log(`Man U won ${manUnitedWins} games`);
+// console.log(`Man U won ${manUnitedWins} games`);
