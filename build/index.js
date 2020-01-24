d86c189f0723314cb6fec8e816f69634e7774b93
Elliot Gonzalez <elliotgrey@gmail.com>
2020-01-24 15:31:10 -0500

    added enum reference

--- build/index.js
+++ build/index.js
@@ -1,2 +1,29 @@
 "use strict";
-console.log('on');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var fs_1 = __importDefault(require("fs"));
+var matches = fs_1.default
+    .readFileSync('football.csv', { encoding: 'utf-8' })
+    .split('\n')
+    .map(function (row) {
+    return row.split(',');
+});
+var MatchResult;
+(function (MatchResult) {
+    MatchResult["HomeWin"] = "H";
+    MatchResult["AwayWin"] = "A";
+    MatchResult["Draw"] = "D";
+})(MatchResult || (MatchResult = {}));
+var manUnitedWins = 0;
+for (var i = 0; i < matches.length; i++) {
+    var match = matches[i];
+    if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+        manUnitedWins++;
+    }
+    else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+        manUnitedWins++;
+    }
+}
+console.log("Man U won " + manUnitedWins + " games");
